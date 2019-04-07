ebb4fdb265f656fac87f9f33e88ff675c3d2895f
J-Falconetti <joseph.falconetti@gmail.com>
2019-04-06 21:36:58 -0400

    finished server.js

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,25 @@
-// models burger.js file
+// models burger.js filevar orm = require("../config/orm.js");
+
+var orm = require("../config/orm.js");
+
+
+var burgers = {
+    all: function(cb) {
+      orm.selectAll("burgers", function(res) {
+        cb(res);
+      });
+    },
+    create: function(cols, vals, cb) {
+      orm.insertOne("burgers", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    update: function(objColVals, condition, cb) {
+      orm.updateOne("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    },
+  };
+  
+  // Export the database functions for the controller.
+  module.exports = burgers;
