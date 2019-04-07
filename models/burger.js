430045ee102ed69e9f3cac70ddeb7cbd62043768
J-Falconetti <joseph.falconetti@gmail.com>
2019-04-07 18:19:17 -0400

    after tutor

--- models/burger.js
+++ models/burger.js
@@ -5,17 +5,17 @@ var orm = require("../config/orm.js");
 
 var burgers = {
     all: function(cb) {
-      orm.selectAll("burgers", function(res) {
+      orm.all("burgers", function(res) {
         cb(res);
       });
     },
     create: function(cols, vals, cb) {
-      orm.insertOne("burgers", cols, vals, function(res) {
+      orm.create("burgers", cols, vals, function(res) {
         cb(res);
       });
     },
     update: function(objColVals, condition, cb) {
-      orm.updateOne("burgers", objColVals, condition, function(res) {
+      orm.update("burgers", {devoured:true}, condition, function(res) {
         cb(res);
       });
     },
