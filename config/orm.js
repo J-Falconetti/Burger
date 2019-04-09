fc3d927dcaf2ed968f41c44c52cbdb8cd4c1d595
J-Falconetti <joseph.falconetti@gmail.com>
2019-04-09 14:45:47 -0400

    testing

--- config/orm.js
+++ config/orm.js
@@ -17,12 +17,10 @@ function objToSql(ob) {
     var value = ob[key];
     // check to skip hidden properties
     if (Object.hasOwnProperty.call(ob, key)) {
-      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
       if (typeof value === "string" && value.indexOf(" ") >= 0) {
         value = "'" + value + "'";
       }
-      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-      // e.g. {sleepy: true} => ["sleepy=true"]
+     
       arr.push(key + "=" + value);
     }
   }
@@ -37,8 +35,8 @@ var orm = {
    all: function(tableInput, cb) {
      var queryString = "SELECT * FROM " + tableInput + ";";
      connection.query(queryString, function(err, result) {
-       if (err) {
-         throw err;
+       if (err.fatal) {
+         throw err.fatal;
        }
        cb(result);
      });
